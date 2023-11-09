5ad3e9cb1e29fce540c068ec83fbb2a4997e0b2f
DELL <manasajalagam379@gmail.com>
2023-11-09 18:07:33 +0530

    expense-shell

---
+++ vars.sh
@@ -0,0 +1,13 @@
+# If we name some data it is called as variable
+# syntax to declare variable
+
+#var_name = data
+
+a = 1
+x = 10
+y =  true
+name- Devops
+
+# How to access variable
+echo value of x = $a
+
